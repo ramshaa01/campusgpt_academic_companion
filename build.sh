4da8e883f034ff73ccadce1d6cd44d5bd54fb011
ramshaa01 <ramshafatimaaa@gmail.com>
2026-06-16 18:43:01 +0530

    Configure Vercel to install Flutter SDK and build web output

--- build.sh
+++ build.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-# Clone the Flutter repository (stable channel)
-if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable
-fi
 
-# Add flutter to PATH
+echo "Downloading Flutter..."
+git clone https://github.com/flutter/flutter.git -b stable
+
+echo "Adding Flutter to PATH..."
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Enable web support
-flutter config --enable-web
-
-# Get dependencies
-flutter pub get
-
-# Build the web app
-flutter build web
+echo "Building Flutter Web App..."
+flutter build web --release
